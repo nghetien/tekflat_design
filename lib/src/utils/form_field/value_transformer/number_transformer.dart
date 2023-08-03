a22e4ee7e4a7a605390cfbed2fd812f9d7e01c42
Tiến Nghê Quyết <tien.nq@teko.vn>
2023-08-03 18:58:46 +0700

    try catch currencyToNumber

--- lib/src/utils/form_field/value_transformer/number_transformer.dart
+++ lib/src/utils/form_field/value_transformer/number_transformer.dart
@@ -4,6 +4,6 @@ class TekNumberTransformer {
   static num? currencyToNumber(String? value) {
     if (value == null || value.isEmpty) return null;
     if (value == '-') return 0;
-    return num.parse(value.replaceAll(',', '').trim());
+    return num.tryParse(value.replaceAll(',', '').trim());
   }
 }
